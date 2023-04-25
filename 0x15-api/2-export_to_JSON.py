013693168483395c0c7916b6f6e5461b4cbe29e5
Benonking <benonkighoma@gmail.com>
2023-04-25 10:36:15 +0300

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
-
+"""script that fetches info about a given employee using an api
+and exports it in json format
 """
-Python script that exports data in the JSON format.
-"""
 
-from requests import get
-from sys import argv
+
+import csv
 import json
+import requests
+from sys import argv
+
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
+    user_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
+    user_dict = requests.get(user_url).json()
+    user_name = user_dict.get("username")
+    user_todo = requests.get("{}/todos".format(user_url))
+    user_todo = user_todo.json()
+    file_name = user_id + ".json"
+    my_dict = {}
 
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+    my_dict[user_id] = []
 
-    for i in data2:
-        if i['id'] == int(argv[1]):
-            u_name = i['username']
-            id_no = i['id']
+    for elem in user_todo:
+        inner_dict = {}
+        inner_dict["task"] = elem.get("title")
+        inner_dict["completed"] = elem.get("completed")
+        inner_dict["username"] = user_name
+        my_dict.get(user_id).append(inner_dict)
 
-    row = []
-
-    for i in data:
-
-        new_dict = {}
-
-        if i['userId'] == int(argv[1]):
-            new_dict['username'] = u_name
-            new_dict['task'] = i['title']
-            new_dict['completed'] = i['completed']
-            row.append(new_dict)
-
-    final_dict = {}
-    final_dict[id_no] = row
-    json_obj = json.dumps(final_dict)
-
-    with open(argv[1] + ".json",  "w") as f:
-        f.write(json_obj)
+    with open(file_name, 'w') as f:
+        json.dump(my_dict, f)
