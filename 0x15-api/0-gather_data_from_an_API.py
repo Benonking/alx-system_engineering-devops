6b14aebe043b2055b4e8ed27444975352ec1ff9b
Benonking <benonkighoma@gmail.com>
2023-04-25 08:42:59 +0300

    gather data from AP1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""
-python script that uses REST API,
-for a given employee Id
-Returns: To do list progresss
-"""
+"""fetching json data from an api"""
+
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    ID = argv[1]
-    user_urli = "https://jsonplaceholder.typicode.com/users/" + ID
-
+    user_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     user_dict = requests.get(user_url).json()
     user_name = user_dict.get("name")
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_todo = requests.get(todo_url).json()
-
-    task_todo = 0
-    task_complete = 0
+    user_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user_todo = user_todo.json()
+    total_todo = 0
     completed_titles = []
+    number_completed = 0
 
     for item in user_todo:
-        if item.get('ID') == int(ID):
-            task_todo += 1
+        if item.get("userId") == int(user_id):
+            total_todo += 1
             if item.get("completed") is True:
-                task_complete += 1
+                number_completed += 1
                 completed_titles.append(item.get("title"))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user_name, task_complete, task_todo))
-
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name, number_completed, total_todo))
     for title in completed_titles:
         print("\t {}".format(title))
