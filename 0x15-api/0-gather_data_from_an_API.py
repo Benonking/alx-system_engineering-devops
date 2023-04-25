b965881cb33218b6a6adbd542894f6200897566e
Benonking <benonkighoma@gmail.com>
2023-04-25 08:32:08 +0300

    gather data from AP1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,13 @@ from sys import argv
 
 if __name__ == "__main__":
     ID = argv[1]
-    user_url="https://jsonplaceholder.typicode.com/users/" + ID
-    
+    user_urli = "https://jsonplaceholder.typicode.com/users/" + ID
+
     user_dict = requests.get(user_url).json()
     user_name = user_dict.get("name")
     todo_url = "https://jsonplaceholder.typicode.com/todos"
     user_todo = requests.get(todo_url).json()
-    
+
     task_todo = 0
     task_complete = 0
     completed_titles = []
@@ -26,7 +26,8 @@ if __name__ == "__main__":
             if item.get("completed") is True:
                 task_complete += 1
                 completed_titles.append(item.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(user_name, task_complete, task_todo))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, task_complete, task_todo))
 
     for title in completed_titles:
-            print("\t {}".format(title))
+        print("\t {}".format(title))
