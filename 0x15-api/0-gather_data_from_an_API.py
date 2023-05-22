9e6fd6155912cb630ee9b1f531cabe085669a4fe
Benonking <benonkighoma@gmail.com>
2023-05-22 16:52:48 +0300

    fix data fetch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that, using a REST API, for a given employee ID,
-"""
-from requests import get
-from sys import argv
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    todos = get(url + 'todos', params={"userId": argv[1]}).json()
-    user = get(url + 'users/{}'.format(argv[1])).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user.get('name'), len(completed), len(todos)))
-    [print('\t {}'.format(t)) for t in completed]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
