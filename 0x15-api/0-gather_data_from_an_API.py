b5771130882b9d3fac4158041fa6c4c1f20f9237
Benonking <benonkighoma@gmail.com>
2023-05-22 16:42:31 +0300

    fix data fetch

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """
 Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
 """
 from requests import get
 from sys import argv
 
-
 if __name__ == "__main__":
-    ID = argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
-    todos = get(url + 'todos', params={"userId": ID}).json()
-    user = get(url + 'users/{}'.format(ID)).json()
+    todos = get(url + 'todos', params={"userId": argv[1]}).json()
+    user = get(url + 'users/{}'.format(argv[1])).json()
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-
     print("Employee {} is done with tasks({}/{}):"
           .format(user.get('name'), len(completed), len(todos)))
     [print('\t {}'.format(t)) for t in completed]
