b23eabac3b3e42f85ef30426494abec1625ac062
Benonking <benonkighoma@gmail.com>
2023-06-06 13:07:23 +0300

    number of subscribers/subeddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+module 0-subs
+query Reddit API and retun the number of subscribers
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    return number of subscribers on the Reddit API
+    '''
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "linux"
+    }
+    r = requests.get(url, headers=headers, allow_redirects=False).json()
+    sub = r.get('data').get('subscribers')
+    if not sub:
+        return 0
+    return sub
