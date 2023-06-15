afdf06bbd7d64d198d205ef359b8063c1895ec00
Benonking <benonkighoma@gmail.com>
2023-06-15 12:00:34 +0300

    inrease traffic

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,10 +4,10 @@
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/g" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 # Restart nginx
-exec { 'nginx-restart':
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
